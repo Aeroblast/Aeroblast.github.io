119fef992d1868b807d9daccb2b397b26c7f0c82
Aeroblast <15964528+Aeroblast@users.noreply.github.com>
2019-09-10 13:45:41 +0800

    0

---
+++ Blog/post.py
@@ -0,0 +1,26 @@
+import  os
+import sys
+import datetime
+from shutil import copyfile
+blogdir= sys.path[0]
+postdata=sys.argv[1]
+if postdata.endswith('.atxt')is False:
+    print('wrong ext')
+    os.system("pause")
+    sys.exit(1)
+dt= datetime.datetime.now()
+message=dt.strftime('%Y%m%d %H:%M:%S')
+filename=dt.strftime('%Y%m%d_%H%M')
+filepath=blogdir+'/Text/'+filename+'.atxt'
+copyfile(postdata, filepath)
+
+a=open(filepath, 'a') 
+a.write('\r\n[time]'+message+'[/time]')
+a.close()
+
+index=blogdir+'/index.txt'
+a=open(index, 'a') 
+a.write('\r\n'+filename)
+a.close()
+print('End')
+os.system("pause")
