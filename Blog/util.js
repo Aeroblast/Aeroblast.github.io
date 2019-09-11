6614784e8ea5fc5045bcc5f817560b65122ec95f
Aeroblast <15964528+Aeroblast@users.noreply.github.com>
2019-09-11 18:08:10 +0800

    0

--- Blog/util.js
+++ Blog/util.js
@@ -3,3 +3,7 @@ function GetQueryString(name) {
     var r = window.location.search.substr(1).match(reg);
     if (r != null) return decodeURIComponent(r[2]); return null;
 }
+function RandomQuery()
+{
+    return "random=" + (new Date()).getTime();
+}
